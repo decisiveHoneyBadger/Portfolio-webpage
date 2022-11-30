8aa07802cb90cd5cf7e2340b53fc6482d2a04fa2
decisiveHoneyBadger <k.guevenc@web.de>
2022-11-30 20:59:27 +0100

    enhanced UI (work) resytled grid items cards added code for alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 function myFunction(x) {
-  x.classList.toggle("change");
-} 
+  x.classList.toggle('change');
+}
+
+// approaches of alerts for the submit button
+let alertMessage = document.querySelector('#submit-button');
+let userName = '';
+
+Element.addEventListener('click' function (e) {
+  alert(`Hey ${userName}, thanks for your message. I will soon get in touch with you!`);
+});
+
+// option 2
+alertMessage.onclick = function () {
+  alert(`Hey ${userName}, thanks for your message. I will soon get in touch with you!`);
+}
+
+// sweetalert alternative
+Swal.fire('Good job!', 'You clicked the button!', 'success');
